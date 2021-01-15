cc1f9224502eb58dda6ba439b1cc0ff952ac9b6d
sukovf <msoucek@seznam.cz>
2021-01-15 10:55:26 +0100

    Sender bank fix

--- src/Abo.php
+++ src/Abo.php
@@ -6,20 +6,24 @@ use snoblucha\Abo\Account\File;
 
 class Abo {
 	const HEADER = 'UHL1';
+
+	/** @var File[] */
 	private $items = array();
+
 	private $organization;
 	private $date;
 	private $comittent_number = 0;
 	private $fixedKeyPart = null;
 	private $securityCode = null;
-	
-	public function __construct($organization = ""){		
+	private $senderBank = '';
+
+	public function __construct($organization = ""){
 		$this->setOrganization($organization);
 		$this->setDate();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Set the organization name. Less then 20 chars.
 	 * @param string $organization
 	 */
@@ -27,11 +31,23 @@ class Abo {
 		$this->organization = $organization;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 * @param string $bankCode
+	 *
+	 * @return $this
+	 */
+	public function setSenderBank($bankCode)
+	{
+		$this->senderBank = $bankCode;
+
+		return $this;
+	}
+
+	/**
+	 *
 	 * Optional part of the header. Set the Fixed key part and security code
-	 * @param int $fixed -  6 numbers 
+	 * @param int $fixed -  6 numbers
 	 * @param int $securityCode 6 numbers
 	 * @return abo
 	 */
@@ -40,28 +56,28 @@ class Abo {
 		$this->securityCode = $securityCode;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Set date of file
 	 * @param string $date format DDMMYY
 	 */
 	public function setDate($date = null){
 		if($date == null) {
-			$date = date('dmy');	
+			$date = date('dmy');
 		}
 		$this->date = $date;
-		return $this;		
-		
+		return $this;
+
 	}
-	
+
 	public function setComittentNumer($number){
 		$this->comittent_number = $number;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Enter description here ...
 	 * @param int $type - 1501 : platebni prikaz , 1502 inkaso  abo_account_file::UHRADA|abo_account_file::INKASO
 	 * @return File
@@ -71,9 +87,9 @@ class Abo {
 		$this->items[] = $item;
 		$item->setNumber(count($this->items));
 		return $item;
-		
+
 	}
-	
+
 	/**
 	 * Get tha account files
 	 * @return array of abo_account_file
@@ -81,29 +97,29 @@ class Abo {
 	public function getFiles(){
 		return $this->items;
 	}
-	
+
 	public function generate(){
 		$res = sprintf("%s%s% -20s%010d%03d%03d",self::HEADER, $this->date, $this->organization, $this->comittent_number,1,1+count($this->items));
 		if($this->securityCode){
 			$res .= sprintf("%06d%06d", $this->fixedKeyPart, $this->securityCode);
 		}
 		$res.= "\r\n";
-		
+
 		foreach ($this->items as $item) {
-			$res .= $item->generate();
+			$res .= $item->generate(true, $this->senderBank);
 		}
-				
+
 		return $res;
-		
+
 	}
-	
+
 	public static function account($number, $pre = null){
 		$res = '';
 		if($pre){
-				$res.= sprintf("%s-",$pre);
-			}
+			$res.= sprintf("%s-",$pre);
+		}
 		$res .= sprintf("%s",  $number);
 		return $res;
 	}
-	
+
 }
--- src/Account/File.php
+++ src/Account/File.php
@@ -3,39 +3,43 @@
 namespace snoblucha\Abo\Account;
 
 use snoblucha\Abo\Group;
+use snoblucha\Abo\Item;
 
 class File {
-	
-	const UHRADA = 1501; 
+
+	const UHRADA = 1501;
 	const INKASO = 1502;
-	 
+
 	private $number = 0;
 	private $type = self::UHRADA;
 	private $bank = 0;
 	private $bankDepartment = 0;
-	
+
+	/** @var Item[] */
 	private $items = array();
-	
+
 	public function __construct($type = self::UHRADA){
 		$this->type = $type;
 	}
-	
+
 	/**
-	 * 
-	 * Generate string, 
+	 *
+	 * Generate string,
+	 * @param string $senderBank
 	 * @return string
 	 */
-	public function generate(){
+	public function generate($senderBank = '')
+	{
 		$res = sprintf("1 %04d %03d%03d %04d\r\n",$this->type, $this->number, $this->bankDepartment, $this->bank);
 		foreach ($this->items as $item) {
-			$res.= $item->generate();			
+			$res.= $item->generate(true, $senderBank);
 		}
 		$res .= "5 +\r\n";
 		return $res;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Set the bank deparment - pobocka. 0 in general
 	 * @param int $number
 	 * @return File
@@ -44,9 +48,9 @@ class File {
 		$this->bankDepartment = $number;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * set number of file. Should be called only from abo
 	 * @param unknown_type $number
 	 * @return File
@@ -55,21 +59,21 @@ class File {
 		$this->number = $number;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Nastavit typ
 	 * @param int $type 1501 - uhrady, 1502 - inkasa
-	 
+
 	 */
 	public function setType($type){
 		$this->type = $type;
 		return $this;
 	}
-	
-	/**	 
+
+	/**
 	 * nastavit kod banky, ktere se dany soubor tyka(ktere to posilame?)
-	 * 
+	 *
 	 * @param int/string $bankCode kod banky
 	 * @return File
 	 */
@@ -77,10 +81,10 @@ class File {
 		$this->bank = $bankCode;
 		return $this;
 	}
-	
+
 	/**
 	 * Add a group to item and return it to set up
-	 * 
+	 *
 	 * @return Group
 	 */
 	public function addGroup(){
